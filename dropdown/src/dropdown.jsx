7b47138c450120b342248abd8464001fba9b16b6
Brittan McGinnis <konamax123@gmail.com>
2015-09-20 15:53:42 -0700

    initial commit

---
+++ dropdown/src/dropdown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var Button = require('./button');
+//var List = require('./list');
+
+module.exports = React.createClass({
+   render: function() {
+       return(
+       <div className="dropdown">
+           <Button className="btn-default" title={this.props.title} subTitleClassName="caret"/>
+       </div>
+       )
+   }
+});
