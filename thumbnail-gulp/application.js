73e285490ea17322f9908cfcb12a29edf8007176
Brittan McGinnis <konamax123@gmail.com>
2015-09-22 20:35:20 -0700

    reformats some code

--- thumbnail-gulp/application.js
+++ thumbnail-gulp/application.js
@@ -1,5 +1,5 @@
 var options = {
-    thumbnailData:  [
+    thumbnailData: [
         {
             header: "Great kitten pics",
             title: 'See Kittens!',
@@ -20,33 +20,38 @@ var options = {
 var element = React.createElement(ThumbnailList, options);
 
 React.render(element, document.querySelector('.container'));
-var Badge = React.createClass({displayName: "Badge",
-    render: function(){
-    return React.createElement("button", {className: "btn btn-primary", type: "button"}, 
-        this.props.title, " ", React.createElement("span", {className: "badge"}, this.props.number)
-)
-}
-});
-var ThumbnailList = React.createClass({displayName: "ThumbnailList",
+
+var Badge = React.createClass({
+    displayName: "Badge",
     render: function () {
-        var list = this.props.thumbnailData.map(function(thumbnailProps){
-            return React.createElement(Thumbnail, React.__spread({},  thumbnailProps))
+        return React.createElement("button", {className: "btn btn-primary", type: "button"},
+            this.props.title, " ", React.createElement("span", {className: "badge"}, this.props.number)
+        )
+    }
+});
+
+var ThumbnailList = React.createClass({
+    displayName: "ThumbnailList",
+    render: function () {
+        var list = this.props.thumbnailData.map(function (thumbnailProps) {
+            return React.createElement(Thumbnail, React.__spread({}, thumbnailProps))
         });
 
-        return React.createElement("div", null, 
+        return React.createElement("div", null,
             list
         )
     }
 });
-var Thumbnail = React.createClass({displayName: "Thumbnail",
-    render: function() {
-        return React.createElement("div", {className: "col-sm-6 col-md-4"}, 
-            React.createElement("div", {className: "thumbnail"}, 
-                React.createElement("img", {src: this.props.imgUrl, alt: "..."}), 
-                React.createElement("div", {className: "caption"}, 
-                    React.createElement("h3", null, this.props.header), 
-                    React.createElement("p", null, this.props.description), 
-                    React.createElement("p", null, 
+var Thumbnail = React.createClass({
+    displayName: "Thumbnail",
+    render: function () {
+        return React.createElement("div", {className: "col-sm-6 col-md-4"},
+            React.createElement("div", {className: "thumbnail"},
+                React.createElement("img", {src: this.props.imgUrl, alt: "..."}),
+                React.createElement("div", {className: "caption"},
+                    React.createElement("h3", null, this.props.header),
+                    React.createElement("p", null, this.props.description),
+                    React.createElement("p", null,
                         React.createElement(Badge, {title: this.props.title, number: this.props.number})
                     )
                 )
--- thumbnail-gulp/src/badge.jsx
+++ thumbnail-gulp/src/badge.jsx
@@ -2,9 +2,9 @@ var React = require('react');
 
 
 module.exports = React.createClass({
-    render: function(){
-    return <button className="btn btn-primary" type="button">
-        {this.props.title} <span className="badge">{this.props.number}</span>
-</button>
-}
+    render: function () {
+        return <button className="btn btn-primary" type="button">
+            {this.props.title} <span className="badge">{this.props.number}</span>
+        </button>
+    }
 });
